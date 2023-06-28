fc873b5bcf15b1177521a23fef022e219eab31d5
Martin Ostadal <ostadalmartin96@gmail.com>
2023-06-28 17:30:36 +0200

    fix(tests): :white_check_mark: import path changed

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,8 @@
 import unittest
 
-from main import new_function
+from ..main import new_function
 
 
 class TestMain(unittest.TestCase):
     def test_main(self):
         self.assertEqual(new_function(), "Hello World!")
-        
