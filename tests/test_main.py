b646573e38f6b8e1d3e83f1723addd6157f421cb
Martin Ostadal <ostadalmartin96@gmail.com>
2023-06-28 17:09:23 +0200

    test(tests): :white_check_mark: Test folder with tests added
    Tests commit
    
    We have tests!

---
+++ tests/test_main.py
@@ -0,0 +1,9 @@
+import unittest
+
+from main import new_function
+
+
+class TestMain(unittest.TestCase):
+    def test_main(self):
+        self.assertEqual(new_function(), "Hello World!")
+        
