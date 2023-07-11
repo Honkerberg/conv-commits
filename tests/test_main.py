e3a4283c72c421a58d6400c66c8d91f7ffa8eafa
Martin Ostadal <ostadalmartin96@gmail.com>
2023-07-11 16:42:25 +0200

    fix(tests): :white_check_mark: __name__ = '__main__' added

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,3 +6,6 @@ from main import new_function
 class TestMain(unittest.TestCase):
     def test_main(self):
         self.assertEqual(new_function(), "Hello World!")
+
+if __name__ == '__main__':
+    unittest.main()
